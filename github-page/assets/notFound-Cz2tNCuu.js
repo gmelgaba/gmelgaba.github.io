22e021342b599dc73966d0f8605f266ba03b81e4
Gonzalo Melgar <gmelgaba@gmail.com>
2025-04-06 09:27:50 -0300

    v4-react app implementation, react-routing github update

--- github-page/assets/notFound-Cz2tNCuu.js
+++
@@ -1,1 +0,0 @@
-import"./main-KOC8rlZl.js";
--- v4-react/gmelgaba-profile-app/vite.config.ts
+++ v4-react/gmelgaba-profile-app/vite.config.ts
@@ -1,22 +1,7 @@
-import { dirname, resolve } from 'path';
-
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
-
-// simulate __dirname for ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        notFound: resolve(__dirname, 'index.html'), // duplicate for 404.html
-      },
-    },
-  },
 });
