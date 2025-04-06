8b89d2f02a9ac76ca4e78e5d5775ee6eb2aa5d33
Gonzalo Melgar <gmelgaba@gmail.com>
2025-04-06 02:39:09 -0300

    v4-react app implementation, step 5 - 404 update

---
+++ github-page/assets/notFound-Cz2tNCuu.js
@@ -0,0 +1,1 @@
+import"./main-KOC8rlZl.js";
--- v4-react/gmelgaba-profile-app/vite.config.ts
+++ v4-react/gmelgaba-profile-app/vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { dirname, resolve } from 'path';
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
+
+// simulate __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   base: '/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notFound: resolve(__dirname, 'index.html'), // duplicate for 404.html
+      },
+    },
+  },
+});
